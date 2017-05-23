85533372d6739ad5f10ef24731c523529f510330
Haide Jocelyn Haaz Leon <16470271@itcampeche.edu.mx>
2017-05-23 15:57:17 -0500

    Modificacion del metodo Main.

--- src/ecuaciondesegundogrado/Main.java
+++ src/ecuaciondesegundogrado/Main.java
@@ -5,6 +5,8 @@
  */
 package ecuaciondesegundogrado;
 
+import modelo.EcuacionDeSegundoGrado;
+
 /**
  *
  * @author sams
@@ -15,7 +17,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        EcuacionDeSegundoGrado ecu1 = new EcuacionDeSegundoGrado(1,-2,18);
+      
+
+       
+            System.out.println("Valos de Discriminante: " + ecu1.getDiscriminate());
+            System.out.println("Numero de Raices: " + ecu1.getNumeroDeSoluciones());
+            System.out.println("X1: " + ecu1.getRaiz1());
+            System.out.println("X2: " + ecu1.getRaiz2());
+
+        EcuacionDeSegundoGrado ecu2 = new EcuacionDeSegundoGrado(1,-9,18);
+           
+            System.out.println("Valos de Discriminante: " + ecu2.getDiscriminate());
+            System.out.println("Numero de Raices: " + ecu2.getNumeroDeSoluciones());
+            System.out.println("X1: " + ecu2.getRaiz1());
+            System.out.println("X2: " + ecu2.getRaiz2());
+        
+       EcuacionDeSegundoGrado ecu3 = new EcuacionDeSegundoGrado(1,6,9); 
+       
+            System.out.println("Valos de Discriminante: " + ecu3.getDiscriminate());
+            System.out.println("Numero de Raices: " + ecu3.getNumeroDeSoluciones());
+            System.out.println("X1: " + ecu3.getRaiz1());
+            System.out.println("X2: " + ecu3.getRaiz2());    
+            
     }
     
 }
--- src/modelo/EcuacionDeSegundoGrado.java
+++ src/modelo/EcuacionDeSegundoGrado.java
@@ -11,4 +11,74 @@ package modelo;
  */
 public class EcuacionDeSegundoGrado {
     
+    private double  a;
+    private double  b;
+    private double  c;
+    
+    public EcuacionDeSegundoGrado (double a, double b, double c){
+        
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    
+    }
+    
+    
+ 
+    public double getCoeficienteA (){
+    
+        return this.a;
+    }
+    
+    
+    public double getCoeficienteB (){
+    
+        return this.b;
+    }
+    
+    public double getCoeficienteC (){
+    
+        return this.c;
+    }
+    
+    public double getDiscriminate (){
+        
+        double discriminate;
+        discriminate = Math.pow(this.b, 2) - 4*this.a*this.c; 
+       
+        return discriminate; 
+    }
+    
+    public int getNumeroDeSoluciones (){
+        
+        int NRaices;
+        double d = getDiscriminate ();
+        if (d > 0){
+          
+            NRaices = 2;
+        }else if (d < 0 ){
+            
+            NRaices = 0;
+        }else {
+            NRaices = 1;
+        }
+      
+        return NRaices;
+    }
+    
+    public double getRaiz1 (){
+        
+        double raiz1;
+        raiz1 = (- this.b + Math.sqrt( getDiscriminate ()))/ (2*this.a);
+        
+        return raiz1;
+    }
+    
+    public double getRaiz2 (){
+        
+        double raiz2;
+        raiz2 = (- this.b - Math.sqrt( getDiscriminate ()))/ (2*this.a);
+        
+        return raiz2;
+    }
 }
